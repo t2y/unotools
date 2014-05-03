de99a07da20a12fdadd5e04719ebe58701ac882b
Tetsuya Morimoto <96569+t2y@users.noreply.github.com>
2014-05-03 15:40:18 +0900

    refactoring for Calc class

--- unotools/calc.py
+++ unotools/calc.py
@@ -1,12 +1,118 @@
 # -*- coding: utf-8 -*-
 from operator import methodcaller
 
+from com.sun.star.awt import Rectangle
+from com.sun.star.chart import XChartDocument
+from com.sun.star.chart import XDiagram
+from com.sun.star.sheet import XCellRangeAddressable
 from com.sun.star.sheet import XSpreadsheet
 from com.sun.star.sheet import XSpreadsheets
+from com.sun.star.table import CellRangeAddress
 from com.sun.star.table import XCellRange
+from com.sun.star.table import XTableChart
+from com.sun.star.table import XTableCharts
 from com.sun.star.util import XNumberFormats
 
 from unotools.component import Component
+from unotools.datatypes import Sequence
+
+
+class ChartDocument(Component):
+
+    def __init__(self, chart_document: XChartDocument):
+        self.raw = chart_document
+
+    def set_diagram(self, diagram: XDiagram):
+        self.raw.setDiagram(diagram)
+
+    def get_diagram(self) -> XDiagram:
+        return self.raw.getDiagram()
+
+
+class TableChart(Component):
+
+    def __init__(self, chart: XTableChart):
+        self.raw = chart
+
+    def get_embedded_object(self) -> ChartDocument:
+        return ChartDocument(self.raw.getEmbeddedObject())
+
+
+class Spreadsheet(Component):
+
+    # sheet operation
+    def __init__(self, sheet: XSpreadsheet):
+        self.raw = sheet
+
+    def set_rows_cell_data(self, x: int, y: int, data: list, method_name: str):
+        for datum in data:
+            methodcaller(method_name, datum)(self.raw.getCellByPosition(x, y))
+            y += 1
+
+    def set_columns_cell_data(self, x: int, y: int, data: list,
+                              method_name: str):
+        for datum in data:
+            methodcaller(method_name, datum)(self.raw.getCellByPosition(x, y))
+            x += 1
+
+    def set_rows(self, x: int, y: int, data: list, method_names: str):
+        for i, datum in enumerate(data):
+            self.set_rows_cell_data(x, y, [datum], method_names[i])
+            x += 1
+
+    def set_columns(self, x: int, y: int, data: list, method_names: str):
+        for i, datum in enumerate(data):
+            self.set_rows_cell_data(x, y, [datum], method_names[i])
+            y += 1
+
+    def set_rows_str(self, x: int, y: int, data: list):
+        self.set_rows_cell_data(x, y, data, 'setString')
+
+    def set_columns_str(self, x: int, y: int, data: list):
+        self.set_columns_cell_data(x, y, data, 'setString')
+
+    def set_rows_value(self, x: int, y: int, data: list):
+        self.set_rows_cell_data(x, y, data, 'setValue')
+
+    def set_columns_value(self, x: int, y: int, data: list):
+        self.set_columns_cell_data(x, y, data, 'setValue')
+
+    def set_rows_formula(self, x: int, y: int, data: list):
+        self.set_rows_cell_data(x, y, data, 'setFormula')
+
+    def set_columns_formula(self, x: int, y: int, data: list):
+        self.set_columns_cell_data(x, y, data, 'setFormula')
+
+    def get_cell_range_by_name(self, range_: str) -> XCellRange:
+        return self.raw.getCellRangeByName(range_)
+
+    def get_cell_range_by_position(self, left: int, top: int, right: int,
+                                   bottom: int) -> XCellRange:
+        return self.raw.getCellRangeByPosition(left, top, right, bottom)
+
+    def get_range_address(self, cell_range: XCellRange
+                          ) -> XCellRangeAddressable:
+        return cell_range.getRangeAddress()
+
+    # charts operation
+    @property
+    def charts(self) -> XTableCharts:
+        return self.raw.getCharts()
+
+    def get_charts_count(self) -> int:
+        return self.get_count(self.charts)
+
+    def add_charts_new_by_name(self, name: str,
+                               rect: Rectangle, ranges: CellRangeAddress,
+                               column_headers: bool, row_headers: bool):
+        self.charts.addNewByName(name, rect, Sequence(ranges),
+                                 column_headers, row_headers)
+
+    def get_chart_by_index(self, index: int) -> TableChart:
+        return TableChart(self.get_by_index(self.charts, index))
+
+    def get_chart_by_name(self, name: str) -> TableChart:
+        return TableChart(self.get_by_name(self.charts, name))
 
 
 class Calc(Component):
@@ -16,19 +122,19 @@ class Calc(Component):
     # sheets operation
     @property
     def sheets(self) -> XSpreadsheets:
-        return self.component.getSheets()
-
-    def get_sheets_by_index(self, index: int) -> XSpreadsheet:
-        return self.sheets.getByIndex(index)
-
-    def get_sheets_by_name(self, name: str) -> XSpreadsheet:
-        return self.sheets.getByName(name)
+        return self.raw.getSheets()
 
     def get_sheets_count(self) -> int:
-        return self.sheets.getCount()
+        return self.get_count(self.sheets)
+
+    def get_sheet_by_index(self, index: int) -> Spreadsheet:
+        return Spreadsheet(self.get_by_index(self.sheets, index))
+
+    def get_sheet_by_name(self, name: str) -> Spreadsheet:
+        return Spreadsheet(self.get_by_name(self.sheets, name))
 
     def get_number_formats(self) -> XNumberFormats:
-        return self.component.getNumberFormats()
+        return self.raw.getNumberFormats()
 
     def insert_sheets_new_by_name(self, name: str, position: int):
         self.sheets.insertNewByName(name, position)
@@ -39,60 +145,3 @@ class Calc(Component):
 
     def remove_sheets_by_name(self, name: str):
         self.sheets.removeByName(name)
-
-    # a sheet operation
-    def set_rows(self, sheet: XSpreadsheet, x: int, y: int, data: list,
-                 method_names: str):
-        for i, datum in enumerate(data):
-            self.set_rows_cell_data(sheet, x, y, [datum], method_names[i])
-            x += 1
-
-    def set_columns(self, sheet: XSpreadsheet, x: int, y: int, data: list,
-                    method_names: str):
-        for i, datum in enumerate(data):
-            self.set_rows_cell_data(sheet, x, y, [datum], method_names[i])
-            y += 1
-
-    def set_rows_cell_data(self, sheet: XSpreadsheet, x: int, y: int,
-                           data: list, method_name: str):
-        for datum in data:
-            methodcaller(method_name, datum)(sheet.getCellByPosition(x, y))
-            y += 1
-
-    def set_columns_cell_data(self, sheet: XSpreadsheet, x: int, y: int,
-                              data: list, method_name: str):
-        for datum in data:
-            methodcaller(method_name, datum)(sheet.getCellByPosition(x, y))
-            x += 1
-
-    def set_rows_str(self, sheet: XSpreadsheet, x: int, y: int, data: list):
-        self.set_rows_cell_data(sheet, x, y, data, 'setString')
-
-    def set_columns_str(self, sheet: XSpreadsheet, x: int, y: int, data: list):
-        self.set_columns_cell_data(sheet, x, y, data, 'setString')
-
-    def set_rows_value(self, sheet: XSpreadsheet, x: int, y: int, data: list):
-        self.set_rows_cell_data(sheet, x, y, data, 'setValue')
-
-    def set_columns_value(self, sheet: XSpreadsheet, x: int, y: int,
-                          data: list):
-        self.set_columns_cell_data(sheet, x, y, data, 'setValue')
-
-    def set_rows_formula(self, sheet: XSpreadsheet, x: int, y: int,
-                         data: list):
-        self.set_rows_cell_data(sheet, x, y, data, 'setFormula')
-
-    def set_columns_formula(self, sheet: XSpreadsheet, x: int, y: int,
-                            data: list):
-        self.set_columns_cell_data(sheet, x, y, data, 'setFormula')
-
-    def get_cell_range_by_name(self, sheet: XSpreadsheet, range_: str
-                               ) -> XCellRange:
-        return sheet.getCellRangeByName(range_)
-
-    def get_cell_range_by_position(self, sheet: XSpreadsheet, left: int,
-                                   top: int, right: int, bottom: int
-                                   ) -> XCellRange:
-        return sheet.getCellRangeByPosition(left, top, right, bottom)
-
-    # charts operation
--- unotools/component.py
+++ unotools/component.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from com.sun.star.lang import XComponent
 from com.sun.star.uno import XComponentContext
+from com.sun.star.uno import XInterface
 
 from unotools.datatypes import Sequence
 
@@ -12,19 +14,42 @@ class Component:
                  arguments: tuple=()):
         self.context = context
         loader = context.load_component_from_url
-        self.component = loader(self.URL, target_frame_name, search_flags,
-                                arguments)
+        self.raw = loader(self.URL, target_frame_name, search_flags, arguments)
 
     def close(self):
-        self.component.close(True)
+        self.raw.close(True)
+
+    def as_raw(self) -> XComponent:
+        return self.raw
+
+    def get_string(self) -> str:
+        return self.raw.getString()
+
+    def set_string(self, text: str):
+        self.raw.setString(text)
+
+    def get_by_index(self, obj: object, index: int) -> object:
+        return obj.getByIndex(index)
+
+    def get_by_name(self, obj: object, name: str) -> object:
+        return obj.getByName(name)
+
+    def get_count(self, obj: object) -> int:
+        return obj.getCount()
+
+    def get_title(self):
+        return self.raw.getTitle()
+
+    def create_instance(self, name: str) -> XInterface:
+        return self.raw.createInstance(name)
 
     def store_as_url(self, url: str, *values):
-        self.component.storeAsURL(url, self._get_property_values(*values))
+        self.raw.storeAsURL(url, self._get_property_values(*values))
 
     def store_to_url(self, url: str, *values):
-        self.component.storeToURL(url, self._get_property_values(*values))
+        self.raw.storeToURL(url, self._get_property_values(*values))
 
-    def _get_property_values(self, *values):
+    def _get_property_values(self, *values) -> Sequence:
         if len(values) == 1 and values[0] is None:
             return Sequence()
         else:
