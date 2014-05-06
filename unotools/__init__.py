182a025c32427c63d729d7e6a9f5f511ac77121a
Tetsuya Morimoto <96569+t2y@users.noreply.github.com>
2014-05-06 19:44:22 +0900

    added command line argument (-f, --file) to able to read OpenDocument file

--- unotools/__init__.py
+++ unotools/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
-from os.path import isdir
+from os.path import isdir, isfile
 
 try:
     from functools import singledispatch
@@ -57,19 +57,22 @@ def parse_argument(argv: list):
     """
     >>> argv = '-s server -p 8080 -a tcpNoDelay=1'.split()
     >>> parse_argument(argv)  # doctest: +NORMALIZE_WHITESPACE
-    Namespace(datadir=None, encoding='utf-8', host='server',
+    Namespace(datadir=None, encoding='utf-8', file_=None, host='server',
               option='tcpNoDelay=1', outputdir='.', pipe=None, port=8080,
               verbose=False)
     """
-    def validate_directory_existence(args):
-        for path in [args.datadir, args.outputdir]:
-            if path is not None and not isdir(path):
-                raise ArgumentError('Directory is not found: {}'.format(path))
+    def validate_file_or_directory_existence(args):
+        def validate(paths, validate_func):
+            for path in paths:
+                if path is not None and not validate_func(path):
+                    raise ArgumentError('"{}" is not found'.format(path))
+        validate([args.file_], isfile)
+        validate([args.datadir, args.outputdir], isdir)
 
     parser = argparse.ArgumentParser()
-    parser.set_defaults(encoding='utf-8', host='localhost', datadir=None,
-                        outputdir='.', option=None, pipe=None, port=8100,
-                        verbose=False)
+    parser.set_defaults(datadir=None, encoding='utf-8', file_=None,
+                        host='localhost', option=None, outputdir='.',
+                        pipe=None, port=8100, verbose=False)
     parser.add_argument('-a', '--option', dest='option',
                         metavar='OPTION', help='set option')
     parser.add_argument('-d', '--datadir', dest='datadir',
@@ -77,6 +80,8 @@ def parse_argument(argv: list):
     parser.add_argument('-e', '--encoding', dest='encoding',
                         metavar='ENCODING',
                         help='set encoding. default is utf-8')
+    parser.add_argument('-f', '--file', dest='file_',
+                        metavar='FILE', help='set OpenDocument file to read')
     parser.add_argument('-i', '--pipe', dest='pipe',
                         metavar='PIPE', help='set pipe name')
     parser.add_argument('-o', '--outputdir', dest='outputdir',
@@ -88,7 +93,7 @@ def parse_argument(argv: list):
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='set verbose mode')
     args = parser.parse_args(argv)
-    validate_directory_existence(args)
+    validate_file_or_directory_existence(args)
 
     if args.verbose:
         logging.root.setLevel(logging.DEBUG)
